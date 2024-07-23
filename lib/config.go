3273dfd660a1495619b565a55cc7f66206f33094
Henrique Dias <mail@hacdias.com>
2024-07-23 07:56:24 +0200

    feat!: simplified rule with regex instead of boolean
    BREAKING CHANGE: the "regex" field in the rule is now a regular expression instead of a boolean.

--- lib/config.go
+++ lib/config.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
@@ -90,7 +91,11 @@ func ParseConfig(filename string, flags *pflag.FlagSet) (*Config, error) {
 	}
 
 	cfg := &Config{}
-	err = v.Unmarshal(cfg)
+	err = v.Unmarshal(cfg, viper.DecodeHook(mapstructure.ComposeDecodeHookFunc(
+		mapstructure.StringToTimeDurationHookFunc(),
+		mapstructure.StringToSliceHookFunc(","),
+		mapstructure.TextUnmarshallerHookFunc(),
+	)))
 	if err != nil {
 		return nil, err
 	}
